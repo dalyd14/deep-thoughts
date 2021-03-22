380263b0a12849a4d8a03bca3d9b32c480c34642
dalyd14 <dalyd14@gmail.com>
2021-03-22 15:05:00 -0400

    added concurrently and others; created MERN functionality by connecting front to back

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const path = require('path')
 const { ApolloServer } = require('apollo-server-express')
+const { authMiddleware } = require('./utils/auth')
 
 const { typeDefs, resolvers } = require('./schemas')
 const db = require('./config/connection');
@@ -9,13 +11,22 @@ const app = express();
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 })
 
 server.applyMiddleware({ app })
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')))
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"))
+})
 
 db.once('open', () => {
   app.listen(PORT, () => {
